63f599049126e8e11fc83ad659f02bcf5cfb5265
Marcos Felipe Silva Celestino <marcos.felipe@timesharesolucoes.com.br>
2023-05-24 06:18:36 -0300

    update

---
+++ Produtos.Infraestrutura/Data/ProdutosContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Produtos.Infraestrutura.Data;
+
+public class ProdutosContext : DbContext
+{
+    public ProdutosContext(DbContextOptions options) : base(options) {}
+}
